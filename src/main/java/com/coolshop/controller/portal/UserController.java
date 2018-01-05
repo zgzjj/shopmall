88629e3103297c35e76c7533943dabd7078bd1f3
zgzjj <252070739@qq.com>
2018-01-05 14:58:08 +0800

    更新用户信息功能

--- src/main/java/com/coolshop/controller/portal/UserController.java
+++ src/main/java/com/coolshop/controller/portal/UserController.java
@@ -80,11 +80,28 @@ public class UserController {
         return iUserService.forgetResetPassword(username, passwordNew, forgetToken);
     }
 
+    @RequestMapping(value = "reset_password.do", method = RequestMethod.GET)
+    @ResponseBody
     public ServerResponse<String> resetPassword(HttpSession session, String passwordOld, String passwordNew) {
         User user = (User) session.getAttribute(Const.CURRENT_USER);
         if (user == null) {
             return ServerResponse.createByErrorMessage("用户未登录");
         }
-        return null;
+        return iUserService.resetPassword(passwordOld, passwordNew, user);
+    }
+
+    @RequestMapping(value = "update_information.do", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<User> update_information(HttpSession session, User user) {
+        User currentUser = (User) session.getAttribute(Const.CURRENT_USER);
+        if (currentUser == null) {
+            return ServerResponse.createByErrorMessage("用户未登录");
+        }
+        user.setId(currentUser.getId());
+        ServerResponse<User> response = iUserService.updateInformation(user);
+        if (response.isSuccess()) {
+            session.setAttribute(Const.CURRENT_USER, response.getData());
+        }
+        return response;
     }
 }
